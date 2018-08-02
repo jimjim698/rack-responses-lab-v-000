a2d84c86bbc742575e53213a81a78e1c67386c33
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-02 19:16:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,13 @@ class Application
     time = Time.new
     resp.write "#{time}\n"
 
+    if time.hour > 12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end 
+
     resp.finish
   end
-  
+
 end
